2bcc1e209d31f65c127cab23bd554a3be01c3786
BuzkoYaroslav <yarik10.98@gmail.com>
2017-09-26 00:27:39 +0300

    add existing numerical analysis library

---
+++ IntegralApproximation/Support classes/Approximator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    static class Approximator
+    {
+        
+        #region Main methods for equasion
+       
+        public static int SolutionCount(Equasion eq, double a, double b)
+        {
+            return 0;
+        }
+        public static Vector ShturmMethod(Equasion eq)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
