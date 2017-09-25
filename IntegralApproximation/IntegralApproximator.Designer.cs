f8a1abc8f8dc19dcbe7ae95f90d8124e25c518ee
BuzkoYaroslav <yarik10.98@gmail.com>
2017-09-26 00:28:30 +0300

    implement integral approximation methods; implement graphical representation of these methods

---
+++ IntegralApproximation/IntegralApproximator.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace IntegralApproximation
+{
+    partial class IntegralApproximator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(644, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(645, -1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(229, 460);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Function";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(205, 24);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Left rectangulars method";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(215, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Right rectangulars method";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(228, 24);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Central rectangulars method";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 115);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(160, 24);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Traperoids method";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 145);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(147, 24);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Simpson method";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 175);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(132, 24);
+            this.radioButton6.TabIndex = 1;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Gauss method";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 255);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(211, 78);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(6, 205);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(216, 24);
+            this.radioButton7.TabIndex = 4;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "simple curvilinear trapezoid";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // IntegralApproximator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 460);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "IntegralApproximator";
+            this.Text = "IntegralApproximator";
+            this.Load += new System.EventHandler(this.IntegralApproximator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton7;
+    }
+}
+
---
+++ IntegralApproximation/IntegralApproximator.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using library;
+
+namespace IntegralApproximation
+{
+    public partial class IntegralApproximator : Form
+    {
+        Random rnd = new Random();
+
+        GraphicPainter[] painters = new GraphicPainter[7];
+        double[] solutions = new double[6];
+
+        MathFunction func = new PowerFunction(1.0d, new XFunction(1.0d), 3);
+
+        private readonly double a = 0;
+        private readonly double b = 2.3;
+
+        private readonly double width = 3;
+        private readonly int n = 10;
+
+        public IntegralApproximator()
+        {
+            InitializeComponent();
+        }
+
+        private void IntegralApproximator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(func.ToString());
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton1.Checked)
+                return;
+
+            if (painters[0] != null)
+            {
+                painters[0].UpdateUI();
+                richTextBox1.Text = solutions[0].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[0]);
+            painters[0].Draw(func, GetRandColor(), (float)width);
+            painters[0].DrawPath(painters[0].PathForRectangularMethod(func, RectangularMethodType.Left, a, b, n), GetRandColor(), (float)width, GetRandColor(), 0.5);
+
+            solutions[0] = func.CalculateDeterminedIntegral(new RectangularMethod(RectangularMethodType.Left), a, b, n);
+            richTextBox1.Text = solutions[0].ToString();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton2.Checked)
+                return;
+
+            if (painters[1] != null)
+            {
+                painters[1].UpdateUI();
+                richTextBox1.Text = solutions[1].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[1]);
+            painters[1].Draw(func, GetRandColor(), (float)width);
+            painters[1].DrawPath(painters[1].PathForRectangularMethod(func, RectangularMethodType.Right, a, b, n), GetRandColor(), (float)width, GetRandColor(), 0.5);
+
+            solutions[1] = func.CalculateDeterminedIntegral(new RectangularMethod(RectangularMethodType.Right), a, b, n);
+            richTextBox1.Text = solutions[1].ToString();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton3.Checked)
+                return;
+
+            if (painters[2] != null)
+            {
+                painters[2].UpdateUI();
+                richTextBox1.Text = solutions[2].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[2]);
+            painters[2].Draw(func, GetRandColor(), (float)width);
+            painters[2].DrawPath(painters[2].PathForRectangularMethod(func, RectangularMethodType.Central, a, b, n), GetRandColor(), (float)width, GetRandColor(), 0.5);
+
+            solutions[2] = func.CalculateDeterminedIntegral(new RectangularMethod(RectangularMethodType.Central), a, b, n);
+            richTextBox1.Text = solutions[2].ToString();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton4.Checked)
+                return;
+
+            if (painters[3] != null)
+            {
+                painters[3].UpdateUI();
+                richTextBox1.Text = solutions[3].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[3]);
+            painters[3].Draw(func, GetRandColor(), (float)width);
+            painters[3].DrawPath(painters[3].PathForTrapezoidMethod(func, a, b, n), GetRandColor(), (float)width, GetRandColor(), 0.5);
+
+            solutions[3] = func.CalculateDeterminedIntegral(new TrapezoidMethod(), a, b, n);
+            richTextBox1.Text = solutions[3].ToString();
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton5.Checked)
+                return;
+
+            if (painters[4] != null)
+            {
+                painters[4].UpdateUI();
+                richTextBox1.Text = solutions[4].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[4]);
+            painters[4].Draw(func, GetRandColor(), (float)width);
+            painters[4].DrawPath(painters[4].PathForSimpsonMethod(func, a, b, n), GetRandColor(), (float)width, GetRandColor(), 0.5);
+
+            solutions[4] = func.CalculateDeterminedIntegral(new SimpsonMethod(), a, b, n);
+            richTextBox1.Text = solutions[4].ToString();
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton6.Checked)
+                return;
+
+            if (painters[5] != null)
+            {
+                painters[5].UpdateUI();
+                richTextBox1.Text = solutions[5].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[5]);
+        }
+
+        private void ConfigurePainter(out GraphicPainter painter)
+        {
+            painter = new GraphicPainter(pictureBox1);
+
+            double maxF = func.MaxValue(a, b, false),
+                   minF = func.MinValue(a, b, false);
+
+            double x, y, x1, y1;
+
+            if (a < 0)
+                x = a - 1;
+            else
+                x = -1;
+            if (b < 0)
+                x1 = 1;
+            else
+                x1 = b + 1;
+
+            if (maxF < 0)
+                y1 = 1;
+            else
+                y1 = maxF + 1;
+
+            if (minF > 0)
+                y = -1;
+            else
+                y = minF - 1;
+
+            painter.XBounds = new Point((int)x - 1, (int)x1 + 1);
+            painter.YBounds = new Point((int)y - 1, (int)y1 + 1);
+        }
+        private Color GetRandColor()
+        {
+            return Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton7.Checked)
+                return;
+
+            if (painters[6] != null)
+            {
+                painters[6].UpdateUI();
+                //richTextBox1.Text = solutions[6].ToString();
+                return;
+            }
+
+            ConfigurePainter(out painters[6]);
+            painters[6].Draw(func, GetRandColor(), (float)width);
+            painters[6].DrawPath(painters[6].PathForFunction(func, a, b), GetRandColor(), (float)width, GetRandColor(), 0.5);
+        }
+    }
+}
